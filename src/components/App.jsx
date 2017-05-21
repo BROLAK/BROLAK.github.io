71540eb0a9a49e7260e5b4070c0e1cd36bc212e0
Mati <matiborlak@gmail.com>
2017-05-21 21:28:42 +0300

    fix script loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ export default class App extends Component {
     constructor(props) {
     	super(props);
     	this.state = {
-            slideIndex: 1
+            slideIndex: 0
     	}
         this.handleSlide = this.handleSlide.bind(this);
   	}
--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,22 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import theme from './theme';
 import App from './components/App';
 import './styles/app.scss';
+
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-ReactDOM.render(
+function run() {
+  ReactDOM.render(
 	<MuiThemeProvider muiTheme={theme}>
    		<App />
-   	</MuiThemeProvider>
-   ,
-   document.getElementById('root')
-);
+   	</MuiThemeProvider>,document.getElementById('root')
+   	);
+}
+
+const loadedStates = ['complete', 'loaded', 'interactive'];
+
+if (loadedStates.includes(document.readyState) && document.body) {
+  run();
+} else {
+  window.addEventListener('DOMContentLoaded', run, false);
+};
