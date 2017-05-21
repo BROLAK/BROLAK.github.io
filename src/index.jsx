f895a07960abc07518a58d5d2ea75c4208e72336
Mati <matiborlak@gmail.com>
2017-05-21 21:32:44 +0300

    mroe script fixing

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import theme from './theme';
-import App from './components/App';
 import './styles/app.scss';
+import App from './components/App';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
