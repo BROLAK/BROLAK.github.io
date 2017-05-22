a967fcd64a02032f2e2d99daf3eb316f3b2b7b25
Mati <matiborlak@gmail.com>
2017-05-22 14:00:42 +0300

    only bundle

--- src/components/subcomponents/MyAvatar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Avatar from 'material-ui/Avatar';
-
-const MyAvatar = (props) => (
-  		<Avatar src='src/img/me.jpg' size={props.size} />
-);
-
-export default MyAvatar;
